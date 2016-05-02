f37c2e7c77a69b92ce7b5d8754b90fd2ad3d8fe7
AndreasHolm <aholm99@hotmail.com>
2016-05-02 15:52:31 +0200

    Is working, but does not find all shortest paths.

--- src/graphhopper/floyd.cpp
+++ src/graphhopper/floyd.cpp
@@ -1,37 +1,51 @@
+#include <sstream>
+#include <fstream>
 #include <iostream>
 #include <climits>
 #include <vector>
-#include "node.h"
+#include <string>
+#include <stdlib.h>
+#include "graph.cpp"
+#include "utils.cpp"
+#include "kernel/kernelfactory.cpp"
 
 using namespace std;
 
 int const INF = 999;
 
-vector<vector<int> > floyd(vector<vector<int> > V) {
+void floyd(Graph* g) {
 
-  vector<vector<int> > dist(V.size(), vector<int>(V.size(), INF));
-  vector<vector<vector<int> > > paths(V.size(), vector<vector<int> >(V.size()));
-	 
+  vector<vector<int> > dist(g -> V.size(), vector<int>(g -> V.size(), INF));
+  vector<vector<vector<int> > > paths(g -> V.size(), vector<vector<int> >(g -> V.size()));
+  //vector<vector<vector<vector<int> > > > paths(g -> V.size(), vector<vector<vector<int> > >(g -> V.size()));
+
   cout << "Neighbours: " << endl;
-  //Set dist to own node to 0.
-  for(int v = 0; v < V.size(); v++) {
+  for(int v = 0; v < g -> V.size(); v++) {
     dist[v][v] = 0;
-    //Set dist to neighbours to 1.
-    for(int u = 0; u < V.size(); u++) {
-      dist[u][v] = V[u][v];//1;
-      //cout << u << " " << v << " = " << dist[u][v] << endl;
-      if(dist[u][v] != INF) {
-        paths[u][v].push_back(u);
-	paths[u][v].push_back(v);
+    for(int u = 0; u < g -> V[v] -> adj.size(); u++) {
+      if(g -> V[v] -> adj[u] -> index != v) {
+        dist[g -> V[v] -> adj[u] -> index][v] = 1;
       }
+      paths[g -> V[v] -> adj[u] -> index][v].push_back(g -> V[v] -> adj[u] -> index);
+      paths[g -> V[v] -> adj[u] -> index][v].push_back(v);
     }
+    paths[v][v].push_back(v);
+    paths[v][v].push_back(v);
   }
 
+  /*
+  for(int i = 0; i < dist.size(); i++) {
+    for(int j = 0; j < dist[i].size(); j++) {
+      cout << dist[i][j] << " ";
+    }
+    cout << endl;
+  }
+  */
   cout << "Paths: " << endl;
-  for(int k = 0; k < V.size(); k++) {
-    for(int i = 0; i < V.size(); i++) {
-      for(int j = 0; j < V.size(); j++) {
-        //if(dist[i][j] >= dist[i][k] + dist[k][j] && j != k) {
+  for(int k = 0; k < g -> V.size(); k++) {
+    for(int i = 0; i < g -> V.size(); i++) {
+      for(int j = 0; j < g -> V.size(); j++) {
+	//cout << dist[i][j] << " " << dist[i][k] + dist[k][j] << endl;
 	if(dist[i][j] > dist[i][k] + dist[k][j]) {
           dist[i][j] = dist[i][k] + dist[k][j];
 	  if (paths[i][j].size() != 0) {
@@ -40,12 +54,11 @@ vector<vector<int> > floyd(vector<vector<int> > V) {
 	  //paths[i][j].push_back(i);
 	  for(int q = 0; q < paths[i][k].size(); q++) {
 	    paths[i][j].push_back(paths[i][k][q]);
-	    cout << "k: " << k << " i: " << i << " j: " << j << " q: " << q << endl;
+	    //cout << "k: " << k << " i: " << i << " j: " << j << " q: " << q << endl;
 	  }
 	  paths[i][j].push_back(j);
 	  //cout << k << " " << i << " " << j << " = " << dist[i][j] << endl;
 	}
-	//cout << k + 1 << " " << i + 1 << " " << j + 1 << " = " << dist[i][j] << endl;
       }
     }
   }
@@ -61,41 +74,18 @@ vector<vector<int> > floyd(vector<vector<int> > V) {
     }
     cout << "----" << endl;
   }
-  
-  return dist;
+  //return dist;
 }
 
-int main() {
-  /*
-  int graph[4][4] =
-    { {  0, INF,  -2, INF},
-      {  4,   0,   3, INF},
-      {INF, INF,   0,   2},
-      {INF,  -1,  INF,  0} };
-  */
-  int graph[4][4] =
-    { {  0,   5,   11,  10},
-      {INF,   0,   3, INF},
-      {INF, INF,   0,   1},
-      {INF, INF, INF,   0} };
-
-  vector<vector<int> > newGraph(4,vector<int>(4,0));
-  for(int i = 0; i < newGraph.size(); i++) {
-    for(int j = 0; j < newGraph[i].size(); j++) {
-      newGraph[i][j] = graph[i][j];
-    }
+int main(int argc, char **argv) {
+  LabelType labelType = KernelFactory().getLabelType(argv[2]);
+  vector<Graph*> graphs = loadData(argv[1], labelType);
+  steady_clock::time_point tStart;
+  tStart = steady_clock::now();
+  for(int i = 0; i < graphs.size(); i++) {
+    floyd(graphs[i]);
   }
-
-  vector<vector<int> > sp = floyd(newGraph);
-
-  /*
-  for(int i = 0; i < sp.size(); i++) {
-    for(int j = 0; j < sp[i].size(); j++) {
-      cout << sp[i][j] << " ";
-    }
-    cout << endl;
-  }  
-  */
+  cout << "Graphs done in: " << msPassed(tStart) << " ms\n";
   return 0;
 }
 
